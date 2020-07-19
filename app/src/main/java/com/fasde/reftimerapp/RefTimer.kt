0a6a673b08e7fbb3c0bd8caa7b455e26f27476a4
Fasde <fabianpoker1@web.de>
2020-07-19 18:24:02 +0200

    WIP3

--- app/src/main/java/com/fasde/reftimerapp/RefTimer.kt
+++ app/src/main/java/com/fasde/reftimerapp/RefTimer.kt
@@ -5,74 +5,124 @@ import android.support.wearable.activity.WearableActivity
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 class RefTimer : WearableActivity() {
+
+    lateinit var mainTime: TextView
+    lateinit var overTime: TextView
+    lateinit var mainTimer: Time
+    lateinit var overTimer: Time
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ref_timer_view)
 
         // Set all the Elements form the View
-        val mainTime : TextView = findViewById(R.id.mainTime)
-        val overTime : TextView = findViewById(R.id.overTime)
-        val startButton : FloatingActionButton = findViewById(R.id.startButton)
-        val secondHalfButton : FloatingActionButton = findViewById(R.id.secondHalfButton)
-        val gameRevertButton : FloatingActionButton = findViewById(R.id.gameRevertButton)
-        var mainTimer = Time()
-        var overTimer = Time()
-        var firstHalf = GlobalScope.launch {  }
-        var secondHalf = GlobalScope.launch {  }
+        mainTime = findViewById(R.id.mainTime)
+        overTime = findViewById(R.id.overTime)
+        val startButton: FloatingActionButton = findViewById(R.id.startButton)
+        val secondHalfButton: FloatingActionButton = findViewById(R.id.secondHalfButton)
+        val gameRevertButton: FloatingActionButton = findViewById(R.id.gameRevertButton)
+        mainTimer = Time()
+        overTimer = Time()
+        var firstHalf = GlobalScope.launch { }
+        var secondHalf = GlobalScope.launch { }
+        var firstHalfRunning = false
+        var secondHalfRunning = false
+        var halftime1 = true
+        var gameEnded = false
+        var setToSecondHalf = false
         // Enables Always-on
         setAmbientEnabled()
 
-        startButton.setOnClickListener{
-            // start the timer
-            firstHalf.cancel()
-            firstHalf = GlobalScope.launch {
-                while(true){
-                    mainTimer.addOneSec()
-                    mainTime.text = mainTimer.toString()
-                    if(mainTimer.minutes >= 45){
-                        break
+        GlobalScope.launch {
+            while (true) {
+                delay(990)
+                if (gameEnded) {
+                    if (firstHalf.isActive) firstHalf.cancel()
+                    if (secondHalf.isActive) secondHalf.cancel()
+                } else {
+                    if (!halftime1 && !setToSecondHalf) {
+                        mainTime.text = mainTimer.secondHalf().toString()
+                        overTime.text = overTimer.reset().toString()
+                        setToSecondHalf = true
                     }
-                }
-                while (true){
-                    overTimer.addOneSec()
-                    overTime.text = overTimer.toString()
+                    updateTimes()
                 }
             }
         }
-        secondHalfButton.setOnClickListener {
-            firstHalf.cancel()
-            secondHalf.cancel()
-            mainTime.text = mainTimer.secondHalf().toString()
-            overTime.text = overTimer.reset().toString()
-            secondHalf = GlobalScope.launch {
-                while(true){
-                    mainTimer.addOneSec()
-                    mainTime.text = mainTimer.toString()
-                    if(mainTimer.minutes >= 90){
-                        break
+
+
+        startButton.setOnClickListener {
+            // start the timer
+            gameEnded = false
+            if (halftime1) {
+                if (!firstHalfRunning) {
+                    if (firstHalf.isActive) firstHalf.cancel()
+                    firstHalfRunning = true
+                    firstHalf = GlobalScope.launch {
+                        while (true) {
+                            delay(990)
+                            launch {
+                                tickFirstHalf()
+                            }
+                        }
                     }
                 }
-                while(true){
-                    overTimer.addOneSec()
-                    overTime.text.toString()
+            } else {
+                if (!secondHalfRunning) {
+                    if (secondHalf.isActive) secondHalf.cancel()
+                    secondHalfRunning = true
+                    secondHalf = GlobalScope.launch {
+                        while (true) {
+                            delay(990)
+                            launch {
+                                tickSecondHalf()
+                            }
+                        }
+                    }
                 }
+
+            }
+        }
+        secondHalfButton.setOnClickListener {
+            if (halftime1) {
+                secondHalfRunning = true
+                firstHalfRunning = false
+                if (firstHalf.isActive) firstHalf.cancel()
+                halftime1 = false
             }
         }
 
         gameRevertButton.setOnClickListener {
-            firstHalf.cancel()
-            secondHalf.cancel()
-            mainTime.text = mainTimer.reset().toString()
-            overTime.text = overTimer.reset().toString()
+            gameEnded = true
             // Stop the ticking Timer
         }
 
 
+    }
+
+    private fun tickFirstHalf() {
+        if (mainTimer.minutes < 45) {
+            mainTimer.addOneSec()
+        } else {
+            overTimer.addOneSec()
+        }
+    }
+
+    private fun tickSecondHalf() {
+        if (mainTimer.minutes < 90) {
+            mainTimer.addOneSec()
+        } else {
+            overTimer.addOneSec()
+        }
+    }
+
+    private fun updateTimes() {
+        mainTime.text = mainTimer.toString()
+        overTime.text = overTimer.toString()
     }
 }
